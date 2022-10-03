0cda3d7f9a293ff72dd3c2d6a3d51e59a4d55529
mnaibei <muchajulius@gmail.com>
2022-10-03 12:15:34 +0300

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Mucha Naibei on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
